5127e243de41f499d06a1af3ea7c3697b7089d5b
Dbhuwad <dineshjb1234@gmail.com>
2022-04-03 16:52:59 +0530

    Updates

---
+++ precache-manifest.5c8cf17c862ae54f1580a1b6a2f864e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac18c6da05387293e5e392c2d2f218a",
+    "url": "/madlab/index.html"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/css/2.9be35272.chunk.css"
+  },
+  {
+    "revision": "2c4b38cba0e5c7b29851",
+    "url": "/madlab/static/css/main.e0e55565.chunk.css"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/js/2.903e3fb3.chunk.js"
+  },
+  {
+    "revision": "2c4b38cba0e5c7b29851",
+    "url": "/madlab/static/js/main.cf9b2817.chunk.js"
+  },
+  {
+    "revision": "396f417b8273cab40f0e",
+    "url": "/madlab/static/js/runtime~main.05b0808b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/madlab/static/media/logo.9f7e506d.svg"
+  }
+]);
